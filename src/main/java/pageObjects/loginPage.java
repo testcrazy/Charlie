a26022930a064b3acfb24522b37480181bd4e694
testcrazy <QAJunkie@mail.de>
2019-09-30 09:56:10 +0200

    Validation of account webelements

--- src/main/java/pageObjects/loginPage.java
+++ src/main/java/pageObjects/loginPage.java
@@ -1,11 +1,20 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import resources.base;
 
 public class loginPage extends base {
 
     public WebDriver driver;
+
+    By emailInput = By.id("email");
+    By passwordInput = By.id("password");
+    By loginBtn = By.cssSelector(".appButton");
+    By forgotPassw = By.cssSelector(".forgotPass");
+    By signUp = By.cssSelector(".go-to-register > a:nth-child(1)");
+
 
     public loginPage(WebDriver driver){
 
@@ -15,4 +24,26 @@ public class loginPage extends base {
     public String getTitle() {
         return driver.getTitle();
     }
+
+    public WebElement getEmail(){
+        return driver.findElement(emailInput);
+    }
+
+    public WebElement getPassword(){
+        return driver.findElement(passwordInput);
+    }
+
+    public WebElement getLoginBtn(){
+        return driver.findElement(loginBtn);
+    }
+
+    public WebElement passwordReset(){
+        return driver.findElement(forgotPassw);
+    }
+
+    public WebElement signUp(){
+        return driver.findElement(signUp);
+    }
+
+
 }
--- src/test/java/accountPage.java
+++ src/test/java/accountPage.java
@@ -1,9 +1,58 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.loginPage;
 import resources.base;
 
 public class accountPage extends base {
 
     private static Logger Log = LogManager.getLogger(base.class.getName());
+
+    @Test
+    public void validateemailField(){
+
+        loginPage ap = new pageObjects.loginPage(driver);
+
+        //Validate if email input field is displayed
+        Assert.assertTrue(ap.getEmail().isDisplayed());
+        Log.info("Successfully validated email input field");
+    }
+
+    @Test(priority = 1)
+    public void validatePasswField(){
+        loginPage ap = new pageObjects.loginPage(driver);
+
+        //Validate if password input field is displayed
+        Assert.assertTrue(ap.getPassword().isDisplayed());
+        Log.info("Successfully validated password input field");
+    }
+
+    @Test(priority = 2)
+    public void validateLoginBtn(){
+        loginPage ap = new pageObjects.loginPage(driver);
+
+        //Validate if login btn is displayed
+        Assert.assertTrue(ap.getLoginBtn().isDisplayed());
+        Log.info("Successfully validated login btn");
+    }
+
+    @Test(priority = 3)
+    public void validatePasswReset(){
+        loginPage ap = new pageObjects.loginPage(driver);
+
+        //Validate if password reset link is displayed
+        Assert.assertTrue(ap.passwordReset().isDisplayed());
+        Log.info("Successfully validated password reset link");
+    }
+
+    @Test(priority = 4)
+    public void validatesignUp(){
+        loginPage ap = new pageObjects.loginPage(driver);
+
+        //Validate if signup for link is displayed
+        Assert.assertTrue(ap.signUp().isDisplayed());
+        Log.info("Successfully validated signup for link");
+    }
 }
